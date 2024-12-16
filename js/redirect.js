1d841ca0d521a1f67de975ad743674abcc26b369
JAKHONGIR DARMANOV <97786628+darmanovjakhongir@users.noreply.github.com>
2024-12-16 22:05:49 +0500

    Delete js directory

--- js/redirect.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const signInButton = document.getElementById("signInButton");
-  if (signInButton) {
-    signInButton.addEventListener("click", function (event) {
-      event.preventDefault();
-      window.location.href = "home.html";
-    });
-  }
-});
